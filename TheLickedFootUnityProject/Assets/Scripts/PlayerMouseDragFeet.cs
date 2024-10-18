3683401b867b1534ef5854677a3c06bf4d8d0f31
Maagiz <epicman228@gmail.com>
2024-10-18 22:51:55 +0200

    code

--- TheLickedFootUnityProject/Assets/Scripts/PlayerMouseDragFeet.cs
+++ TheLickedFootUnityProject/Assets/Scripts/PlayerMouseDragFeet.cs
@@ -1,32 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+
 
 public class PlayerMouseDragFeet : MonoBehaviour
 {
-    public Scene ThisScene;
-    public string FootName;
-    public int Number;
+
+    public Vector3 targetPosition; // The position to move towards
+    public Vector3 startingPosition; // The original position of the object
+    public Vector3 test;
+    public float magnitude;
+
+    public float speed; // Speed of movement
+    public float currentSpeed;
+    public float AtoB;
+    public Vector3 AtoB1;
+    public Vector3 FootToTarget;
+    public float FootToTargetMagnitude;
+    public float DistansToGoal;
 
     private Camera TheCamera;
 
-    private Vector3 ThisObjectScreenPoint;
-    private Vector3 MouseScreenPoint;
-
-    // Start is called before the first frame update
     void Start()
     {
-        TheCamera = ThisScene.Gameobject.Find("MainCamera");
+        //camera
+        TheCamera = Camera.main;
+        TheCamera.enabled = true;
+        AtoB1 = targetPosition - startingPosition;
+        AtoB = AtoB1.magnitude;
+    }
+
+    private void OnMouseDown()
+    {
+        if (test == startingPosition)
+        {
+            test = targetPosition;
+        }
+        else
+        {
+            test = startingPosition;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        ThisObjectScreenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        FootToTarget = transform.position - test;
+        FootToTargetMagnitude = FootToTarget.magnitude;
+        DistansToGoal = FootToTarget.magnitude / AtoB;
 
-        MouseScreenPoint = TheCamera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
 
-        transform.x = MouseScreenPoint.Vector3.x;
-        transform.y = MouseScreenPoint.Vector3.y;
+        // Move the object towards the target position
+        transform.position = Vector3.MoveTowards(transform.position, test, speed * Time.deltaTime * (DistansToGoal * 5f));
+        //speed * Time.deltaTime
+
+
     }
 }
